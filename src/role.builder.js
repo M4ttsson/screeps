f009e96b24d44bfe8ffdd57c0c6ed83ecf16f251
M4ttsson <14147055+M4ttsson@users.noreply.github.com>
2020-12-02 14:54:07 +0100

    fix bug

--- src/role.builder.js
+++ src/role.builder.js
@@ -33,7 +33,7 @@ var roleBuilder = {
                     return (structure.structureType == STRUCTURE_CONTAINER) &&
                         structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
             }});
-            if (targets > 0) {
+            if (targets.length > 0) {
                 if(creep.harvest(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffaa00'}});
                 }
@@ -47,7 +47,7 @@ var roleBuilder = {
                             return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_CONTAINER) &&
                                 structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
                     }});
-                    if (targets > 0) {
+                    if (targets.length > 0) {
                         if(creep.harvest(targets[0]) == ERR_NOT_IN_RANGE) {
                             creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffaa00'}});
                         }

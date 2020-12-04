4dfc6dd64cf2c5424458299958d621093ccd9b91
M4ttsson <14147055+M4ttsson@users.noreply.github.com>
2020-12-04 10:34:37 +0100

    new inital grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,46 @@
-let matchdep = require('matchdep');
-let mergeFiles = require('./grunt-scripts/mergeFiles');
-let credentials = require('./credentials.js');
 
 module.exports = function (grunt) {
-	matchdep.filterAll(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
-	mergeFiles(grunt);
+    var config = require('./.screeps.json')
+
+    grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-contrib-clean')
+    grunt.loadNpmTasks('grunt-contrib-copy')
 
 	grunt.initConfig({
 		screeps: {
-			options: credentials,
-			dist:    {
+			options: {
+                email: config.email,
+                password: config.password,
+                branch: config.branch,
+                ptr: config.ptr
+            },
+			dist: {
 				src: ['dist/*.js']
 			}
-		},
+        },
+        // Remove all files from the dist folder.
+        clean: {
+            'dist': ['dist']
+        },
 
-		copy:      {
-			main: {
-				expand:  true,
-				flatten: true,
-				filter:  'isFile',
-				cwd:     'dist/',
-				src:     '**',
-				dest:    'Update This Path'
-			},
-		},
-	});
-
-	grunt.registerTask('main', ['test', 'merge', 'write']);
-	grunt.registerTask('sandbox', ['merge', 'write-private']);
-	grunt.registerTask('merge', 'mergeFiles');
-	grunt.registerTask('write', 'screeps');
-	grunt.registerTask('write-private', 'copy');
+        // Copy all source files into the dist folder, flattening the folder structure by converting path delimiters to underscores
+        copy: {
+            // Pushes the game code to the dist folder so it can be modified before being send to the screeps server.
+            screeps: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: '**',
+                    dest: 'dist/',
+                    filter: 'isFile',
+                    rename: function (dest, src) {
+                        // Change the path name utilize underscores for folders
+                        return dest + src.replace(/\//g,'_');
+                    }
+                }],
+            }
+        },
+          
+    });
+    grunt.registerTask('default',  ['clean', 'copy:screeps', 'screeps']);
 };
--- credentials-template.js
+++
@@ -1,8 +0,0 @@
-// Rename this file to credentials and add user pass etc.
-
-module.exports = {
-    email: 'EMAIL',
-    password: 'PASSWORD',
-    branch: 'default',
-    ptr: false,
-};

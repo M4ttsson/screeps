edf054abc2b8558d7613608f963a4185134771c2
M4ttsson <14147055+M4ttsson@users.noreply.github.com>
2020-12-04 11:11:15 +0100

    add beautify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,28 @@
 
 module.exports = function (grunt) {
+    require('time-grunt')(grunt);
+
+
     var config = require('./.screeps.json')
+
+      // Allow grunt options to override default configuration
+    var branch = grunt.option('branch') || config.branch;
+    var email = grunt.option('email') || config.email;
+    var password = grunt.option('password') || config.password;
+    var ptr = grunt.option('ptr') ? true : config.ptr
 
     grunt.loadNpmTasks('grunt-screeps');
     grunt.loadNpmTasks('grunt-contrib-clean')
     grunt.loadNpmTasks('grunt-contrib-copy')
+    grunt.loadNpmTasks("grunt-jsbeautifier")
 
 	grunt.initConfig({
 		screeps: {
 			options: {
-                email: config.email,
-                password: config.password,
-                branch: config.branch,
-                ptr: config.ptr
+                email: email,
+                password: password,
+                branch: branch,
+                ptr: ptr
             },
 			dist: {
 				src: ['dist/*.js']
@@ -40,7 +50,26 @@ module.exports = function (grunt) {
                 }],
             }
         },
+
+        // Apply code styling
+        jsbeautifier: {
+            modify: {
+                src: ["src/**/*.js"],
+                options: {
+         //           config: '.jsbeautifyrc'
+                }
+            },
+            verify: {
+                src: ["src/**/*.js"],
+                options: {
+                    mode: 'VERIFY_ONLY',
+       //             config: '.jsbeautifyrc'
+                }
+            }
+        }
           
     });
     grunt.registerTask('default',  ['clean', 'copy:screeps', 'screeps']);
+    grunt.registerTask('test',     ['jsbeautifier:verify']);
+    grunt.registerTask('pretty',   ['jsbeautifier:modify']);
 };

a6eb7bc6128a4ddc961ecd21718e44ad8318ef09
M4ttsson <14147055+M4ttsson@users.noreply.github.com>
2020-12-08 14:56:25 +0100

    add drop at container if hauler

--- src/creeps/harvester.js
+++ src/creeps/harvester.js
@@ -21,20 +21,37 @@ var harvester = {
                 });
             }
         } else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_CONTAINER) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+            // check so haulers has spawned 
+            var haulers = _.filter(Game.creeps, (haul) => haul.memory.role == 'hauler' && haul.room.name == creep.room.name);
+            if (haulers.length > 0) {
+                let closestContainer = creep.pos.findClosestByRange(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0});
+                if (closestContainer) {
+                    console.log("Dropping at closest container");
+                    if (creep.transfer(closestContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(closestContainer, {
+                            visualizePathStyle: {
+                                stroke: '#ffffff'
+                            }
+                        });
+                    }
                 }
-            });
-            if (targets.length > 0) {
-                creep.memory.building = false;
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {
-                        visualizePathStyle: {
-                            stroke: '#ffffff'
-                        }
-                    });
+            }
+            else {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_CONTAINER) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                });
+                if (targets.length > 0) {
+                    creep.memory.building = false;
+                    if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {
+                            visualizePathStyle: {
+                                stroke: '#ffffff'
+                            }
+                        });
+                    }
                 }
             }
             // TODO: Else drop and go back to mining!
